878b63e030d4012e4ff2c6d67a46ffea1c940d43
moisesaraujo2005 <moises.silva@sptech.school>
2024-08-08 08:10:05 -0300

    First commit

---
+++ src/main/java/primeiraClasse.java
@@ -0,0 +1,12 @@
+public class primeiraClasse {
+
+    public static void main(String[] args) {
+
+        String nome = "Bob";
+        Integer idade = 42;
+        Double altura = 1.80;
+
+
+    System.out.println("Olá mundo java: "  + nome +  " minha idade é " + idade + " E altura de " + altura);
+    }
+}
